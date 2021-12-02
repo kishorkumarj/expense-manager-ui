99380c284a5e0593a608bf50abebdfd7868f8546
Kishor K Jagadeesan <kishorkrishnalayam@gmail.com>
2021-12-03 00:34:42 +0530

    Fixes for page-logo

--- src/components/Layout/sidebar.tsx
+++ src/components/Layout/sidebar.tsx
@@ -16,12 +16,14 @@ const SideComponent = () => {
       theme="light"
       width={250}
       className="sidebar-container">
-      <div className="portal-title-container">
+      <div className={collapsed ? "portal-title-container-collapsed" : "portal-title-container"}>
         <img
-          src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
+          src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
           width="30"
           height="30"/>
-          <div className="portal-title">React Client</div>
+          {collapsed ? null : 
+            <div className="portal-title">React Client</div>
+          }
       </div>
       <SideMenu/>
     </Sider>
