69c6b2c00fc3e5ae1033914cfa93f334a78f318c
Kishor K Jagadeesan <kishorkrishnalayam@gmail.com>
2021-12-09 23:35:26 +0530

    Support for mobile view.

--- src/components/Layout/header.js
+++ src/components/Layout/header.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Layout, Avatar, Dropdown, Menu, Input, Badge } from 'antd';
+import React, { useState } from 'react';
+import { Layout, Avatar, Dropdown, Menu, Input, Badge, Drawer } from 'antd';
 import { NavLink } from 'react-router-dom';
 import {
   SearchOutlined,
@@ -7,15 +7,24 @@ import {
   UserOutlined,
   SettingFilled,
   CalendarOutlined,
-  LogoutOutlined
+  LogoutOutlined,
+  MenuOutlined,
+  CloseOutlined
 } from '@ant-design/icons';
 
 import profileImage from '../../assets/images/user_img.jpg'
 import './layout.scss';
+import SideMenu from './menu';
 
 const { Header } = Layout;
 
 const PageHeader = () => {
+
+  const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
+
+  const onMobileMenuClose = () => {
+    setMobileMenuVisible(false);
+  }
 
   const menu = (
     <Menu>
@@ -36,6 +45,19 @@ const PageHeader = () => {
         icon={<LogoutOutlined style={{paddingRight: '3px'}}/>}>Logout</Menu.Item>
     </Menu>
   );
+
+  const siderHeader = (
+    <div className="portal-title-container">
+      <img
+        src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
+        width="30"
+        height="30"/>
+        <div className="portal-title">React Client</div>
+        <CloseOutlined
+          className="mobile-menu-close-icon"
+          onClick={() => setMobileMenuVisible(false)}/>
+    </div>
+  )
 
   return (
     <Header
@@ -68,6 +90,23 @@ const PageHeader = () => {
                 placeholder="Search"
                 className="header-search-box"
                 prefix={<SearchOutlined style={{color: '#cfd5da'}}/>}/>
+                
+                <div className="mobile-menu">
+                  <Drawer
+                    title={siderHeader}
+                    placement="left"
+                    className="mobile-menu-drawer"
+                    visible={mobileMenuVisible}
+                    closable={false}
+                    onClose={onMobileMenuClose}
+                    bodyStyle={{padding: '0px'}}
+                    width={250}>
+                    <SideMenu />
+                  </Drawer>
+                  <MenuOutlined
+                    onClick={() => setMobileMenuVisible(true)}
+                    className="mobile-menu-icon"/>
+                </div>
             </div>
         </div>
       </Header>
